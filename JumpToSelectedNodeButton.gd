7f683d4396609d14c0e2b98fbb337a6edccf05c9
Hyper Sonic <hs@fuwar.in>
2020-04-22 03:32:49 +0100

    Fixed crash when trying to follow node without transform
    Check is node has method "get_global_transform" before calling it
    Small refactoring to avoid repetition

--- JumpToSelectedNodeButton.gd
+++ JumpToSelectedNodeButton.gd
@@ -2,10 +2,16 @@ extends Button
 
 func _on_JumpToSelectedNodeButton_pressed():
 	if get_parent().get_node("ShowNodeInfoButton"):
-		if VDGlobal.visual_debugger.full_selected_path != "":
-			if get_node(VDGlobal.visual_debugger.full_selected_path) != null:
-				var relative_position = get_node(VDGlobal.visual_debugger.full_selected_path).get_global_transform().origin # For speed and convenience.
-				VDGlobal.visual_debugger.set_moving_to_node(true, relative_position)
+		var selected_node_path = VDGlobal.visual_debugger.full_selected_path
+		if selected_node_path != "":
+			var node = get_node(selected_node_path)
+			if node != null:
+				# do not follow nodes without transform
+				if node.has_method("get_global_transform"):
+					var relative_position = node.get_global_transform().origin # For speed and convenience.
+					VDGlobal.visual_debugger.set_moving_to_node(true, relative_position)
+				else:
+					VDGlobal.visual_debugger.is_moving_to_node = false
 			else:
 				print("Oh, noh, nothing to follow, level probably got reloaded!")
 		else:
